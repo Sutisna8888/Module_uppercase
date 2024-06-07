01752696c9cf2283bde7a6b5a3ff072e577910e4
Sutisna8888 <161696845+Sutisna8888@users.noreply.github.com>
2024-06-07 19:13:08 +0700

    Add files via upload

---
+++ Module_UPPERCASE.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def Alfabet(Huruf):
+    jumlah = 0
+    for i in Huruf:
+        if i.isupper():
+            jumlah += 1
+        else :
+            pass
+
+    return jumlah
+
+
+# In[ ]:
+
+
+
+
---
+++ TEKS (1).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import Module_UPPERCASE
+
+kata = "HARI ini Adalah Hari YANG indah"
+
+Hasil = Alfabet(kata)
+
+print(Hasil)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
